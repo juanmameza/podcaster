2775cc8bb61dd63074e25c424de392a65a85b60a
Juan Manuel Meza <j.mezablanco@gmail.com>
2023-08-28 12:34:14 +0200

    feat: define actions

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { PodcastEntry } from "../types";
 
 const initialState = [
@@ -6,13 +6,30 @@ const initialState = [
     podcastList: [],
     selectedPodcast: {},
     selectedEpisode: {},
+    isLoading: false,
   },
 ];
 
 const podcasterSlice = createSlice({
   name: "podcasts",
   initialState,
-  reducers: {},
+  reducers: {
+    loadPodcastList: (state) => {
+      return { ...state, isLoading: true };
+    },
+    loadPodcastListSuccess: (state, action: PayloadAction<PodcastEntry[]>) => {
+      return { ...state, podcastList: action.payload, isLoading: false };
+    },
+    loadPodcastListError: (state) => {
+      return { ...state, podcastList: [], isLoading: false };
+    },
+    selectPodcast: (state, action) => {
+      return { ...state, selectedPodcast: action.payload };
+    },
+    selectEpisode: (state, action) => {
+      return { ...state, selectedEpisode: action.payload };
+    },
+  },
 });
 
 export default podcasterSlice.reducer;
