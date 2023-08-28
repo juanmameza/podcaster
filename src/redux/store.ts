89f24bd06d10037c9a3dfa23d27e81a2bdbef623
Juan Manuel Meza <j.mezablanco@gmail.com>
2023-08-28 12:34:46 +0200

    chore: add saga middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from "@reduxjs/toolkit";
 
-import podcastsReducer from './reducer'
+import podcastsReducer from "./reducer";
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export default configureStore({
   reducer: {
-    posts: podcastsReducer
-  }
-})
+    posts: podcastsReducer,
+  },
+  middleware: [sagaMiddleware],
+});
