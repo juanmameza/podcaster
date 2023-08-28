27936313057f8edbb067607c728a117f5ceeb377
Juan Manuel Meza <j.mezablanco@gmail.com>
2023-08-28 12:34:28 +0200

    chore: add constants for config

---
+++ src/config/constants.ts
@@ -0,0 +1,8 @@
+const CONSTANTS = {
+  PodcastListURL:
+    "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json",
+  EpisodeURL:
+    "https://itunes.apple.com/lookup?id=[id]&media=podcast&entity=podcastEpisode&limit=20",
+};
+
+export default CONSTANTS;
