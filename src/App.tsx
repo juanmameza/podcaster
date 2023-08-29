1403af9d505dc59709f59e1331e53c8afe4c6505
Juan Manuel Meza <j.mezablanco@gmail.com>
2023-08-29 12:44:56 +0200

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ListPage from './pages/List';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ListPage from "./pages/List";
+import { useNavigate } from "react-router-dom";
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
+  const navigate = useNavigate();
   return (
     <div className="App">
+      <Header onClickHome={() => navigate("/")}></Header>
       <ListPage />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+  onClickHome: () => void;
+};
+
+const Header: React.FC<Props> = ({ onClickHome }) => {
+  return (
+    <header>
+      <h1 onClick={onClickHome}>Podcaster</h1>
+    </header>
+  );
+};
+
+export default Header;
