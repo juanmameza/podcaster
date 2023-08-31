b3a33f5da19c980ed7fb2de58c9c512c34165914
Juan Manuel Meza <j.mezablanco@gmail.com>
2023-08-31 12:34:11 +0200

    chore: detail episode type

--- src/types/index.ts
+++ src/types/index.ts
@@ -27,6 +27,71 @@ export type PodcastResponse = {
   };
 };
 
+export type EpisodeWrapper = {
+  wrapperType: string;
+  kind: string;
+  artistId: number;
+  collectionId: number;
+  trackId: number;
+  artistName: string;
+  collectionName: string;
+  trackName: string;
+  collectionCensoredName: string;
+  trackCensoredName: string;
+  artistViewUrl: string;
+  collectionViewUrl: string;
+  feedUrl: string;
+  trackViewUrl: string;
+  artworkUrl30: string;
+  artworkUrl60: string;
+  artworkUrl100: string;
+  collectionPrice: number;
+  trackPrice: number;
+  collectionHdPrice: number;
+  releaseDate: Date;
+  collectionExplicitness: string;
+  trackExplicitness: string;
+  trackCount: number;
+  trackTimeMillis: number;
+  country: string;
+  currency: string;
+  primaryGenreName: string;
+  contentAdvisoryRating: string;
+  artworkUrl600: string;
+  genreIds: string[];
+  genres: string[];
+};
+
+export type Episode = {
+  previewUrl: string;
+  collectionViewUrl: string;
+  feedUrl: string;
+  closedCaptioning: string;
+  collectionId: number;
+  collectionName: string;
+  genres: { name: string; id: string }[];
+  episodeGuid: string;
+  description: string;
+  trackId: number;
+  trackName: string;
+  releaseDate: Date;
+  artistIds: number[];
+  shortDescription: string;
+  episodeUrl: string;
+  country: string;
+  artworkUrl600: string;
+  artworkUrl160: string;
+  episodeFileExtension: string;
+  episodeContentType: string;
+  artworkUrl60: string;
+  artistViewUrl: string;
+  contentAdvisoryRating: string;
+  trackViewUrl: string;
+  trackTimeMillis: number;
+  kind: string;
+  wrapperType: string;
+};
+
 export enum Views {
   Home = "home",
   Podcast = "podcast",
